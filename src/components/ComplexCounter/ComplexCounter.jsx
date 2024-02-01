22e7d4e73ed852a7046e2450fd6b7feea82dde6d
NF RIDOY <nfridoy@gmail.com>
2024-02-02 03:18:46 +0600

    reusabel complex counter uisng reducer

--- src/components/ComplexCounter/ComplexCounter.jsx
+++ src/components/ComplexCounter/ComplexCounter.jsx
@@ -7,9 +7,9 @@ const reducer = (state, action) => {
     // switch state for control as action says
     switch (action?.type) {
         case "increment":
-            return { counter: state?.counter + 1};
+            return { counter: state?.counter + action?.value };
         case "decrement":
-            return { counter: state?.counter - 1};
+            return { counter: state?.counter - action?.value };
         default:
             return state;
     }
@@ -27,15 +27,31 @@ export default function ComplexCounter() {
             <div>
                 <p>Counter {state?.counter}</p>
                 <button onClick={() => dispatch({
-                    type: "increment"
+                    type: "increment",
+                    value: 1
                 })}>
                     Increment
                 </button>
                 <button onClick={() => dispatch({
-                    type: "decrement"
+                    type: "decrement",
+                    value: 1
                 })}>
                     Decrement
                 </button>
+                <div>
+                    <button onClick={() => dispatch({
+                        type: "increment",
+                        value: 10
+                    })}>
+                        Increment 10
+                    </button>
+                    <button onClick={() => dispatch({
+                        type: "decrement",
+                        value: 10
+                    })}>
+                        Decrement 10
+                    </button>
+                </div>
             </div>
         </div>
     )
